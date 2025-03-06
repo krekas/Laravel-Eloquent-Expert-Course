0f2f646f6abae8738c9174879b7c617ce6b83838
Nerijus <krekas@gmail.com>
2025-03-06 14:45:42 +0200

    Lesson 06

---
+++ app/Enums/UserLevel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum UserLevel: int
+{
+    case Junior = 1;
+    case Mid = 2;
+    case Senior = 3;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\UserLevel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_level',
     ];
 
     /**
@@ -43,6 +45,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'user_level' => UserLevel::class,
         ];
     }
 }
